3776fa9ba6b31cebe4e857667cb39e8ab1d7aca8
GalaxyPotato343 <jodielin0304@gmail.com>
2017-10-02 08:14:43 -0500

    Add files via upload

---
+++ Costomer.java
@@ -0,0 +1,19 @@
+public class Costomer{
+	private String name;
+	private String address;
+	private boolean inArea;
+	private String email;
+
+	public Costomer(String name, String address, boolean inArea, String email){
+		this.name = name;
+		this.address = address;
+		this.inArea = inArea;
+		this.email = email;
+	}
+
+	public static String getName(){
+		return this.name;
+	}
+	public static 
+
+}
