3fb988f3388c2416fd111d37c6ea9e725bd5193a
FinnyTheFatFish <32455984+FinnyTheFatFish@users.noreply.github.com>
2017-11-30 09:18:31 -0600

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,51 @@
+package packetlayer;
+import java.sql.*;
+
+public class Database {
+	private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+    
+    public Database() {
+    	 try {
+    	     Class.forName("com.mysql.jdbc.Driver");
+    	     connection = DriverManager.getConnection("jdbc:mysql://66.6.115.212:443/am_packetLayer", "root", "");
+    	     statement = connection.createStatement();
+    	 } catch (Exception ex) {
+    	     System.out.println("Error: " + ex);
+    	     
+    	 }
+    }    
+    public void writeData(){
+    	try {
+    		String query = " insert into users (first_name, last_name, date_created, is_admin, num_points)" + " values (?, ?, ?, ?, ?)";
+    		PreparedStatement preparedStatement = connection.prepareStatement(query);
+    	}catch(Exception ex){
+    		System.out.println(ex);
+    	}
+    }
+    	    
+    public void getData() {
+    	 try {
+    	     String query = "select * from User_List";
+    	     resultSet = statement.executeQuery(query);
+    	     System.out.println("Records from Database");
+    	     while (resultSet.next()) {
+    	    	 String userNumber = resultSet.getString("UserNumber");
+    	    	 String userName = resultSet.getString("UserName");
+    	    	 String password = resultSet.getString("Password");
+    	    	 String userLevel = resultSet.getString("UserLevel");
+
+    	    	 System.out.println("User Number " + userNumber + userName + password + userLevel);
+    	     }
+    	     
+    	 } catch(Exception ex) {
+    	     System.out.println(ex);
+    	 }
+    }
+    
+    public static void main(String[] args){
+    	Database data = new Database();
+    	data.writeData();
+    }
+}
